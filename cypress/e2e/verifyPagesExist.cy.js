5ed6241a2c8aebade056763557604169a612bc1f
filip-stamenkov <stamenkov.filip@gmail.com>
2025-06-03 23:30:38 +0200

    Formatting

--- cypress/e2e/verifyPagesExist.cy.js
+++ cypress/e2e/verifyPagesExist.cy.js
@@ -3,24 +3,24 @@ import { productsP } from "../pages/productsPage";
 
 describe('Verify certain pages exist and can be navigated to', () => {
 
-beforeEach(() => {
-    cy.visit('/');
-    cy.url().should('eq', 'https://automationexercise.com/');
-    // Verify that you are on the home page
-});
+    beforeEach(() => {
+        cy.visit('/');
+        cy.url().should('eq', 'https://automationexercise.com/');
+        // Verify that you are on the home page
+    });
 
-it('Should navigate to and verify the "Test Cases" page', () => {
-// Test Case 7: Verify Test Cases Page
+    it('Should navigate to and verify the "Test Cases" page', () => {
+    // Test Case 7: Verify Test Cases Page
         navBar.verifyTestCasesButtonExistsAndClick();
 
-});
+    });
 
-it.only('Should navigate to and verify the "Products" page', () => {
-// Test Case 8: Verify All Products and product detail page
+    it.only('Should navigate to and verify the "Products" page', () => {
+    // Test Case 8: Verify All Products and product detail page
     navBar.clickProductsButton();
     productsP.containsProducts();
     productsP.clickFirstProduct();
     productsP.verifyProductDetailsVisible();
-});
 
+    });
 });
